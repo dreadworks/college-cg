2435613ab1482d4e16275698c5e7dfa89507ef8b
Felix Hamann <nvri@dreadworks.de>
2014-05-29 22:39:52 +0200

    added Singleton as decorator

--- t2-objv/util.py
+++ t2-objv/util.py
@@ -12,6 +12,9 @@ Utility Module.
 """
 
 
+#
+#   LOGGING
+#
 class Log(object):
 
     def __init__(self, name):
@@ -22,8 +25,8 @@ class Log(object):
                 ' '.join([
                     '[%(levelname)s]',
                     '%(asctime)s -',
-                    '[%(name)s]',
-                    '[%(threadName)s]',
+                    '[%(filename)s:%(lineno)s]',
+                    #'[%(funcName)s]',
                     '%(message)s']),
                 '%H:%M:%S'))
 
@@ -38,3 +41,45 @@ class Log(object):
 
 # initialize
 LOG = Log('objv')
+
+
+#
+#   SINGLETON
+#
+#   (from stackoverflow.com/questions/42558/python-and-the-singleton-pattern)
+class Singleton:
+    """
+    A non-thread-safe helper class to ease implementing singletons.
+    This should be used as a decorator -- not a metaclass -- to the
+    class that should be a singleton.
+
+    The decorated class can define one `__init__` function that
+    takes only the `self` argument. Other than that, there are
+    no restrictions that apply to the decorated class.
+
+    To get the singleton instance, use the `Instance` method. Trying
+    to use `__call__` will result in a `TypeError` being raised.
+
+    Limitations: The decorated class cannot be inherited from.
+    """
+
+    def __init__(self, decorated):
+        self._decorated = decorated
+
+    def Instance(self):
+        """
+        Returns the singleton instance. Upon its first call, it creates a
+        new instance of the decorated class and calls its `__init__` method.
+        On all subsequent calls, the already created instance is returned.
+        """
+        try:
+            return self._instance
+        except AttributeError:
+            self._instance = self._decorated()
+            return self._instance
+
+    def __call__(self):
+        raise TypeError('Singletons must be accessed through `Instance()`.')
+
+    def __instancecheck__(self, inst):
+        return isinstance(inst, self._decorated)
